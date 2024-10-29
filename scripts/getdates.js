f0048ae3d5bcaee60c7dd06a60d88c1f4133cfd2
tenieceb <tabusenbark@gmail.com>
2024-10-29 14:37:56 -0600

    test upload

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+
+
+// const currentyear = document.querySelector('#currentyear');
+// const lastupdated = document.querySelector('#lastModified');
+// const today = new Date();
+// currentyear.innerHTML =`${today.getFullYear()}`;
+// lastupdated.innerHTML = `Last Modified: ${alert(document.lastModifed)}`;
