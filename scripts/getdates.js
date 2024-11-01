b84ddb368e6de131b4c6d742bb3e32366c10d0f0
tenieceb <tabusenbark@gmail.com>
2024-10-31 18:32:43 -0600

    updated homepage

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,6 @@
 
-
-// const currentyear = document.querySelector('#currentyear');
-// const lastupdated = document.querySelector('#lastModified');
-// const today = new Date();
-// currentyear.innerHTML =`${today.getFullYear()}`;
-// lastupdated.innerHTML = `Last Modified: ${alert(document.lastModifed)}`;
+const currentyear = document.querySelector("#currentyear");
+const lastupdated = document.querySelector("#lastModified");
+const today = new Date();
+currentyear.innerHTML =`${today.getFullYear()}`;
+lastupdated.innerHTML = `Last Modified: ${alert(document.lastModifed)}`;
