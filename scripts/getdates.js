7e14bb855824a0bfe909d38e9a089848c4fc81af
tenieceb <tabusenbark@gmail.com>
2024-11-01 13:04:38 -0600

    updated javascript

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,4 +3,6 @@ const currentyear = document.querySelector("#currentyear");
 const lastupdated = document.querySelector("#lastModified");
 const today = new Date();
 currentyear.innerHTML =`${today.getFullYear()}`;
-lastupdated.innerHTML = `Last Modified: ${alert(document.lastModifed)}`;
+let lastModif = new Date(document.lastModified);
+
+lastupdated.innerHTML = `Last Modified:  ${lastModif}`;
